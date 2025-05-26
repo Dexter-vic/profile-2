56187068f78816f14bc05e65d95f7c3d191d85b2
Dexter-vic <victordexter863@gmail.com>
2025-05-26 16:16:39 +0300

    added css file

---
+++ mkdirfirst-project/comparison.py
@@ -0,0 +1,20 @@
+# first_number= input("Enter a number")
+# second_number= input("Enter a number")
+# result= int(first_number) - int(second_number)
+# print(result)
+
+# result2= int(first_number) * int(second_number)
+# result3= int(first_number) / int(second_number)
+# print(result2,result3)
+# number=input("30")
+# number=input("-5")
+# result=int(30)-int(-5)
+# print(result)
+
+
+
+# age=int(input("what is the age"))
+# if age > 18:
+#     print("elegible to vote")
+# else:
+#     print("not eligible to vote")
